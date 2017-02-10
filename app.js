86562526b9809c2aa020605c526fbb607893d49f
Gavin Orland <gavin@gavinorland.com>
2017-02-10 00:38:49 +0000

    Establish basic page

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Gavin on 09/02/2017.
+ */
+
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use(express.static('.'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
